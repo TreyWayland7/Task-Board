02e712b8a14c259710b95fe137199586e9ceb9c4
Trey Wayland <waylandtrey7@gmail.com>
2024-06-29 21:36:48 -0700

    added modal form

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,18 @@
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
 
+
+
+// const addTaskButtonEl = document.querySelector('#addButton')
+// const modalEl = document.querySelector('#modal')
+// const modalEl = document.getElementById("modal");
+const submitTaskButtonEl = document.getElementById("submitTaskButton");
+const inputTaskTitleEl = document.getElementById("inputTaskTitle");
+const inputDateEl = document.getElementById("inputDate");
+const inpu1tContentEl = document.getElementById("inpu1tContent");
+const formModalEl = document.getElementById("formModal");
+
+
 // Todo: create a function to generate a unique task id
 function generateTaskId() {
 
@@ -9,7 +21,8 @@ function generateTaskId() {
 
 // Todo: create a function to create a task card
 function createTaskCard(task) {
-
+    const taskCard = document.createElement('div');
+    document.body.appendChild(taskCard);
 }
 
 // Todo: create a function to render the task list and make cards draggable
@@ -19,7 +32,19 @@ function renderTaskList() {
 
 // Todo: create a function to handle adding a new task
 function handleAddTask(event){
+    event.preventDefault();
+    console.log(inputDateEl.value);
+    console.log(inpu1tContentEl.value);
+    
 
+    $('#formModal').modal('hide');
+
+    const task = {
+        title : inputTaskTitleEl.value,
+        date : inputDateEl.value,
+        content: inpu1tContentEl.value
+    };
+    createTaskCard(task);
 }
 
 // Todo: create a function to handle deleting a task
@@ -31,8 +56,11 @@ function handleDeleteTask(event){
 function handleDrop(event, ui) {
 
 }
-
+// addTaskButtonEl.addEventListener('click', handleAddTask);
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
-
+    
 });
+
+
+formModalEl.addEventListener('submit', handleAddTask);
